2febdf9075aa3b309be1470750452c6eb9d5d0d3
richilazza99 <riccardo.lazzarini@mail.polimi.it>
2023-08-24 12:38:58 +0200

    Optimization find_min_diss

---
+++ Functional motif discovery example/test_min_diss_standalone.R
@@ -0,0 +1,65 @@
+# new test for find_min_diss_rcpp
+library(microbenchmark)
+# caricare data.Rdata
+
+y = list(output_prof$Y0$c1, output_prof$Y1$c1)
+v = output_prof$V0
+
+set.seed(123)  # Set the desired seed value in R for obtaining the same results
+
+alpha = 0.5
+
+use0 = TRUE
+use1 = TRUE
+
+w = 1 
+c_k = 2
+d = 1
+
+# test with distance H1
+output1 = .find_min_diss(y,v,alpha,w,c_k,d,use0,use1)
+output1
+output2 =  find_diss_nicco(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2)
+output2
+output3 = find_diss_efficient(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2)
+output3
+
+# test with distance L2
+
+alpha = 0.0
+use0 = TRUE
+use1 = FALSE
+
+output1 = .find_min_diss(y,v,alpha,w,c_k,d,use0,use1)
+output1
+output2 =  find_diss_nicco(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2)
+output2
+output3 = find_diss_efficient(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2)
+output3
+
+
+
+# test with distance L2 in derivative
+# il problema è qui, non torna 
+
+alpha = 1.0
+use0 = FALSE
+use1 = TRUE
+
+output1 = .find_min_diss(y,v,alpha,w,c_k,d,use0,use1)
+output1
+output2 =  find_diss_nicco(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2)
+output2
+output3 = find_diss_efficient(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2)
+output3
+
+
+# execution time
+
+risultati <- microbenchmark(.find_min_diss(y,v,alpha,w,c_k,d,use0,use1), find_diss_efficient(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2))
+print(risultati)
+  
+tempo_funzione1 <- system.time(.find_min_diss(y,v,alpha,w,c_k,d,use0,use1))
+tempo_funzione2 <- system.time( find_diss_efficient(y,v,w, alpha, c_k, d, use0, use1, .domain, .select_domain, .diss_d0_d1_L2))
+tempo_funzione1 
+tempo_funzione2  
