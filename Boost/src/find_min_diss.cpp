d4bb094e4890e109c2ed8218d7b727071f9abab4
NiccoloF <niccolo.feresini@mail.polimi.it>
2023-08-26 13:28:02 +0200

    add ranges c++20

--- Boost/src/find_min_diss.cpp
+++ Boost/src/find_min_diss.cpp
@@ -5,14 +5,14 @@
 using namespace Rcpp;
 using namespace arma;
 #include <numeric>
-#include <iostream>
-#include <fstream>
+#include <ranges>
+#include <algorithm>
 
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
 //
 // [[Rcpp::depends(RcppArmadillo)]]
-
+// [[Rcpp::plugins(cpp20)]]
 IntegerVector myseq(int first, int last);
 
 // [[Rcpp::export]]
@@ -94,12 +94,12 @@ NumericVector find_diss(const List &y,const List &v,
 
 // more efficient version 
 // [[Rcpp::export]]
-NumericVector find_diss_efficient(const List &y,const List &v,
-                        const NumericVector & w, double alpha, unsigned int c_k,
-                        unsigned int d, bool use0, 
-                        bool use1, const Function & domain, 
-                        const Function & select_domain,
-                        const Function & diss_d0_d1_L2)
+NumericVector find_diss_efficient(const List &y,const List &v,  
+                                  const NumericVector & w, double alpha, unsigned int c_k,
+                                  unsigned int d, bool use0, 
+                                  bool use1, const Function & domain, 
+                                  const Function & select_domain,
+                                  const Function & diss_d0_d1_L2)
 {
   // Convert domain and select_domain
   LogicalVector v_dom = as<LogicalVector>(domain(v,use0));
@@ -107,8 +107,8 @@ NumericVector find_diss_efficient(const List &y,const List &v,
   int v_len = v_dom.size();
   int y_len = as<mat>(y[0]).n_rows;
   IntegerVector s_rep = myseq(1 - (v_len - c_k), y_len - v_len + 1 + (v_len - c_k));
-  
-  List y_rep(s_rep.size());
+  std::size_t s_rep_size = s_rep.size();
+  List y_rep(s_rep_size);
   
   // Convert y[0] and y[1] to mat objects
   mat temp_y0, temp_y1;
@@ -119,36 +119,31 @@ NumericVector find_diss_efficient(const List &y,const List &v,
     temp_y1 = as<mat>(y[1]);
   }
   
-  for (unsigned int i = 0; i < s_rep.size(); i++) {
+  for (unsigned int i = 0; i < s_rep_size; i++) {
     IntegerVector index = s_rep[i] - 1 + seq_len(v_len);
+    const int index_size = index.size();
     List y_rep_i = List::create(Named("y0") = R_NilValue, Named("y1") = R_NilValue);
+    auto j_true = std::views::iota(0,index_size) 
+      | std::views::filter([&index,&y_len](int j){return((index[j] > 0) && (index[j] <= y_len));});
     if (use0) {
-      mat new_y0(index.size(), d);
+      mat new_y0(index_size, d);
       new_y0.fill(datum::nan);
-      for (unsigned int j = 0; j < index.size(); j++) {
-        if ((index[j] > 0) && (index[j] <= y_len)) {
-          new_y0.row(j) = temp_y0.row(index[j] - 1); //TO BE CHECKED 
-        }
-      }
+      std::for_each(j_true.begin(),j_true.end(),[&new_y0,&temp_y0,&index](int j){new_y0.row(j) = temp_y0.row(index[j] - 1);});
       y_rep_i["y0"] = new_y0;
     }
     if (use1) {
-      mat new_y1(index.size(), d);
+      mat new_y1(index_size, d);
       new_y1.fill(datum::nan);
-      for (int j = 0; j < index.size(); j++) {
-        if ((index[j] > 0) && (index[j] <= y_len)) {
-          new_y1.row(j) = temp_y1.row(index[j] - 1); // TO BE CHECKED 
-        }
-      }
+      std::for_each(j_true.begin(),j_true.end(),[&new_y1,&temp_y1,&index](int j){new_y1.row(j) = temp_y1.row(index[j] - 1);});
       y_rep_i["y1"] = new_y1;
     }
     y_rep_i = select_domain(y_rep_i, v_dom, use0, use1);
     y_rep[i] = y_rep_i;
   }
+  const std::size_t y_rep_size = y_rep.size();
+  IntegerVector length_inter(y_rep_size);
   
-  IntegerVector length_inter(y_rep.size());
-  
-  for (int i = 0; i < y_rep.size(); i++) {
+  for (int i = 0; i < y_rep_size; i++) {
     List temp_y_rep_i = as<List>(y_rep[i]);
     mat temp_y;
     if (use0) {
@@ -168,9 +163,9 @@ NumericVector find_diss_efficient(const List &y,const List &v,
   s_rep = s_rep[valid];
   y_rep = y_rep[valid];
   
-  NumericVector d_rep(y_rep.size());
+  NumericVector d_rep(y_rep_size);
   
-  for (int i = 0; i < y_rep.size(); i++) {
+  for (int i = 0; i < y_rep_size; i++) {
     double dist = as<double>(diss_d0_d1_L2(as<List>(y_rep[i]), v_new, w, alpha));
     d_rep[i] = dist;
   }
